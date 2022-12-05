4596f5ed618f70c5c0aed474a5828bd0f66bba16
fadelsaad99 <118170848+fadelsaad99@users.noreply.github.com>
2022-12-05 17:22:42 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
+#include "lib.h"
+bool carattere(char a) {
+    int x;
+    x = (int) a;
+    if (x >= 65 && x <= 90 || x >= 97 && x <= 122) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+char lettera(char a) {
+    int x;
+    x = (int) a;
+    int z = 0;
+    if (x >= 65 && x <= 90) {
+        z = x + 32;
+    } else if (x >= 97 && x <= 122) {
+        z = x - 32;
+    } return z;
+}
